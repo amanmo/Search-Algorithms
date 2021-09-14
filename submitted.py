7c8e887877572e90f6ac2f01b1b7d6f9d88c08ba
Aman Mohandas <aman.mohandas@gmail.com>
2021-09-14 12:55:21 -0700

    Revert "Renamed"
    This reverts commit 3e5163880d2a5e67cf8556695510e0ce747acc33.

--- submitted.py
+++ submitted.py
@@ -1,5 +1,4 @@
 from math import fabs
-import time
 
 class Point:
 
@@ -14,7 +13,7 @@ class Point:
         'Function that returns the next set of coordinates and the cost after a movement takes place from the current point'
 
         x, y, z = self.coordinates
-        cost = 10 if movement < 7 else 14
+        cost = 10 if movement in range(1,7) else 14
 
         #X
         if movement in [1, 7, 8, 11, 12]:
@@ -36,89 +35,12 @@ class Point:
 
         return (x, y, z), cost
 
-class Node:
-
-    def __init__(self, point, path, totalCost, heuristicCost, stepCost):
-        self.point = point
-        self.path = path
-        self.totalCost = totalCost
-        self.heuristicCost = heuristicCost
-        self.stepCost = stepCost
-        self.next = None
-
-class Frontier:
-
-    def __init__(self):
-        self.front = None
-        self.rear = None
-        self.length = 0
-
-    def enqueue(self, point, path, totalCost, heuristicCost, stepCost, algo='BFS'):
-
-        node = Node(point, path, totalCost, heuristicCost, stepCost)
-
-        if self.length == 0:
-            print('self.length =========== 0')
-            self.front = node
-            self.rear = node
-
-        elif algo == 'BFS':
-            print('algo ====== BFS')
-            r = self.rear
-            r.next = node
-            self.rear = node
-
-        else:
-            #revise this else statement
-            test_node = self.front
-            flag = False
-            while test_node is not None:
-                if (algo == 'A*' and test_node.heuristicCost > heuristicCost) or (algo == 'UCS' and test_node.totalCost > totalCost):
-                    flag = True
-                    break
-                test_node = test_node.next
-
-            if flag:
-                if test_node == self.front:
-                    self.front = node
-                node.next = test_node
-            else:
-                if self.front is None:
-                    self.front = node
-                if self.rear is not None:
-                    self.rear.next = node
-                self.rear = node
-
-            # print('else', self.front)
-
-        self.length += 1
-
-    def dequeue(self):
-
-        f = self.front
-        print(f)
-        self.length -= 1
-        if f == self.rear:
-            self.rear = None
-        self.front = f.next
-        return f.point, f.path, f.totalCost, f.heuristicCost, f.stepCost
-
-    def display(self):
-
-        node = self.front
-        x = []
-        while node is not None:
-            x += [node.point.coordinates]
-            node = node.next
-        print(x)
-
-
 class Tunnel:
 
-    def parseInput(self, fname='input.txt'):
+    def parseInput(self):
         'Function to parse the input file'
 
-        f = open(fname)
+        f = open('input.txt')
         self.algo = f.readline().strip()
         self.max_x, self.max_y, self.max_z = [int(i) for i in f.readline().split()]
         self.init = tuple(int(i) for i in f.readline().split())
@@ -167,18 +89,12 @@ class Tunnel:
     def search(self):
         'Function to search for given ending point'
 
-        frontier = Frontier()
-        frontier.enqueue(self.points[self.init], [], 0, 0, [0])
-        frontier.display()
+        frontier = [[self.points[self.init], [], 0, 0, [0]]]
         found = False
         totalCost = 0
-        # total_checker = 0
 
-        while frontier.length != 0:
-            # checker = time.time()
-            # frontier.display()
-            point, path, totalCost, _, stepCost = frontier.dequeue()
-            # total_checker += time.time() - checker
+        while len(frontier) != 0:
+            point, path, totalCost, _, stepCost = frontier.pop(0)
             point.visited = True
             if self.checkGoal(point):
                 found = True
@@ -187,14 +103,19 @@ class Tunnel:
             for movement in point.movements:
                 x, cost = self.getNextPoint(point, movement)
                 if x is not None and x.visited is False:
-                    frontier.enqueue(
+                    frontier += [
+                                    [
                                         x, 
                                         path + [point.coordinates], 
                                         totalCost + 1 if self.algo == 'BFS' else totalCost + cost,
                                         totalCost + cost + self.heuristic(x) if self.algo == 'A*' else 0,
-                                        stepCost + [1] if self.algo == 'BFS' else stepCost + [cost],
-                                        self.algo
-                                    )
+                                        stepCost + [1] if self.algo == 'BFS' else stepCost + [cost]
+                                    ]
+                                ]
+                    if self.algo == 'UCS':
+                        frontier.sort(key=lambda y: y[2])
+                    elif self.algo == 'A*':
+                        frontier.sort(key=lambda y: y[3])
 
         
         self.found = found
@@ -203,12 +124,11 @@ class Tunnel:
             self.path_length = len(path) + 1
             self.final_path = path + [point.coordinates]
             self.stepCost = stepCost
-            # print(total_checker)
 
-    def saveOutput(self, output='output.txt'):
+    def saveOutput(self):
         'Function to save results to output.txt'
 
-        with open(output, 'w') as f:
+        with open('output.txt', 'w') as f:
             if not self.found:
                 f.write('FAIL')
             else:
@@ -218,10 +138,8 @@ class Tunnel:
                 for point in range(len(self.final_path)):
                     f.write('\n' + str(self.final_path[point][0]) + ' ' + str(self.final_path[point][1]) + ' ' + str(self.final_path[point][2]) + ' ' + str(self.stepCost[point]))
 
-def main(input_file='input.txt', output_file='output.txt'):
-    t = Tunnel()
-    t.parseInput(input_file)
-    t.search()
-    t.saveOutput(output_file)
 
-main()
+t = Tunnel()
+t.parseInput()
+t.search()
+t.saveOutput()
